553424cf5f4cecc7198e5ab260c1137f4de7e677
Clearly-Josh <mail2jpclark@gmail.com>
2016-07-06 16:55:04 -0500

    I think I can, I think I can, I think I can, I think I can...
    He lives :)

---
+++ Engine/Inferno.ps1
@@ -0,0 +1,24 @@
+$Command = Read-Host "I shall "
+
+switch($Command){
+    Ignite { Write-Host "In Ignite"
+        While ($true)
+        {
+            # Do things lots
+    
+            #Check sys notifications
+                #if notification, play alert audio
+            #Check sys status
+            #Consider giving inspiration
+                #if long time since event, display something from heart or ask a how-are-you question
+                
+            Write-Host "I think I can"
+        
+            # Add a pause so the loop doesn't run super fast and use lots of CPU        
+            Start-Sleep 30
+        }
+    }
+    default {Write-Host "I must reject your command.  Farewell."
+    }
+
+}
