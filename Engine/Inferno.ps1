7d030e57d006c147fdca498123655e0627811968
Clearly-Josh <mail2jpclark@gmail.com>
2016-07-08 10:29:43 -0500

    Red light, green light
    -Charm can now Ignite and Rest on command

--- Engine/Inferno.ps1
+++ Engine/Inferno.ps1
@@ -1,12 +1,15 @@
-while($true)
+do
 {
-    $Command = Read-Host "I shall "
+    $Command = Read-Host "`nI shall "
 
     switch($Command){
         Ignite {
+            #Play engine noise (random pick between Mad Max intro, Mercedes S-class, Jet engine, starfighter, etc.)
+            #$player = New-Object System.Media.SoundPlayer "$env:windir\Media\notify.wav"
+            
             $ignition = 1
             $turns = 0
-            
+
             do
             {
                 Write-Host "I think I can"
@@ -25,13 +28,23 @@ while($true)
                 }
 
                 # Add a pause so the loop doesn't run super fast and use lots of CPU        
-                Start-Sleep 2
+                Start-Sleep -m 700
                 $turns+=1
                           
             }while($ignition -eq 1)
+
+            $Command = Read-Host "`nFired up.  What can I do?`n"
+            switch($Command){
+                Rest{"And rest I shall, until need calls again.";break}
+                default {Write-Host "I must reject your command."}
+            }
+            
         }
         default {Write-Host "I must reject your command."
         }
     
     }
-}
+
+    if($Command -eq "Rest"){$go = $false}
+
+}while($go -eq $true)
